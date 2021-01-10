fa24444afe1eb15da921475c92bc3961325b8e79
cerebrium <nicholas.m.shankland@gmail.com>
2021-01-09 21:40:11 -0800

    added a little

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+import '../App.scss';
 
 const Home = () => {
+    // auth0
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
     return(
-        <>
+        <div className='homeContainer'>
             <h1>Welcome to the app</h1>
-        </>
+            <h3>{user ? user.email : ''}</h3>
+        </div>
     )
 }
 
